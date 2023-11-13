1652eec189aedffc272699702c33833c041b8cf1
indrabayu-koinworks <indra.bayu@koinworks>
2023-11-13 14:46:43 +0700

    Update like component to stateless functional component

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Component } from "react";
+import React from "react";
 // import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons"; // ES Module "as" syntax
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
@@ -7,33 +7,33 @@ import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 // Input : Liked boolean
 // Output onClick
 
-class Like extends Component {
-  render() {
-    // const solid = <FontAwesomeIcon icon={faHeartSolid} />;
-    // const regular = <FontAwesomeIcon icon={faHeartRegular} />;
-    // const element = this.props.liked ? solid : regular;
-    // return element;
-    // const { liked } = this.props;
-    // const icon = liked ? faHeartSolid : faHeartRegular;
-    // return <FontAwesomeIcon icon={icon} />;
-    return (
-      <FontAwesomeIcon
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        icon={this.props.liked ? faHeartSolid : faHeartRegular}
-      />
-    );
-  }
-}
+// class Like extends Component {
+//   render() {
+// const solid = <FontAwesomeIcon icon={faHeartSolid} />;
+// const regular = <FontAwesomeIcon icon={faHeartRegular} />;
+// const element = this.props.liked ? solid : regular;
+// return element;
+// const { liked } = this.props;
+// const icon = liked ? faHeartSolid : faHeartRegular;
+// return <FontAwesomeIcon icon={icon} />;
+//     return (
+//       <FontAwesomeIcon
+//         onClick={this.props.onClick}
+//         style={{ cursor: "pointer" }}
+//         icon={this.props.liked ? faHeartSolid : faHeartRegular}
+//       />
+//     );
+//   }
+// }
 
-// const Like = () => {
-//   return (
-//     <FontAwesomeIcon
-//       onClick={this.props.onClick}
-//       style={{ cursor: "pointer" }}
-//       icon={this.props.liked ? faHeartSolid : faHeartRegular}
-//     />
-//   );
-// };
+const Like = (props) => {
+  return (
+    <FontAwesomeIcon
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+      icon={props.liked ? faHeartSolid : faHeartRegular}
+    />
+  );
+};
 
 export default Like;
